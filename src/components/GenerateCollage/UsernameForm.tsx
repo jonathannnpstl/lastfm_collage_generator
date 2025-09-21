ba19998ed99c7cbdd6a4197611f0e7090539644c
jonathannnpstl <jonathannnpstl@gmail.com>
2025-09-21 09:05:03 +0800

    removed the dark theme

--- src/components/GenerateCollage/UsernameForm.tsx
+++ src/components/GenerateCollage/UsernameForm.tsx
@@ -50,7 +50,7 @@ const UsernameForm: React.FC<StepProps> = ({ updateSettingsData, nextStep }) => 
           type="text" 
           name="floating_username" 
           id="floating_username" 
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer" 
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer" 
           placeholder=" " 
           required
           value={username || ""}
