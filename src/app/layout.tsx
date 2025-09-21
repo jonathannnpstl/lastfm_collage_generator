51522713642489833a858fa598be2f193d4360a0
jonathannnpstl <jonathannnpstl@gmail.com>
2025-09-21 08:56:49 +0800

    fixed ununsed variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
         <SpeedInsights/>
+        {children}
       </body>
     </html>
   );
